2e840d1b59613d5e692017e3e80ab4e492c5418d
朱敦峰 <2101580047@qq.com>
2019-12-04 23:48:48 +0800

    阿敦

--- src/hellogithub/Hello.java
+++ src/hellogithub/Hello.java
@@ -2,6 +2,6 @@ package hellogithub;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hellogithub");
+		System.out.println("hellogithub,I am your father");
 	}
 }
